991fb2f5008151b97db1f2d21a8678d7c140f0a4
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-08-31 11:28:04 +0530

    import java.util.Scanner; public class Problem {     public static void main (String[] args) {         Scanner scan = new Scanner(System.in);         System.out.println(Integer.parseInt(scan.next(),8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Problem {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(Integer.parseInt(scan.next(),8));
+    }
+}
