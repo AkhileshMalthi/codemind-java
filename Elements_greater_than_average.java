32b6bafee3880975d455a8dbb615cee94d15d07f
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-09-11 17:13:13 +0530

    import java.util.Scanner;
    public class Problem {
        public static void main(String[] args) {
            Scanner scan = new Scanner(System.in);
            int n = scan.nextInt();
            int[] arr = new int[n];
            int sum = 0;
            int c = 0;
            for (int i=0;i<n;i++) {
                arr[i] = scan.nextInt();
                sum += arr[i];
            }
            int avg = sum/n;
            for (int i=0;i<n;i++) {
                if (arr[i] >= avg) c++;
            }
            System.out.println(c);
        }
    }

---
+++ Elements_greater_than_average.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
+        int c = 0;
+        for (int i=0;i<n;i++) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+        int avg = sum/n;
+        for (int i=0;i<n;i++) {
+            if (arr[i] >= avg) c++;
+        }
+        System.out.println(c);
+    }
+}
